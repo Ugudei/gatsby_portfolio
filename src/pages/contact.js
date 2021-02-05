0d258b54223acc7f8c3391322f780b15857fca2b
Ugudei T <ugudeit@mac-machine.local>
2021-02-05 00:19:39 -0700

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
-
+import contact from "../styles/contact.png"
 export default function Contact() {
   return (
     <Layout>
-      <h1>I'd love to talk! Email me at the address below</h1>
+      <img src={contact} />
+      <h1>I'd love to talk about anything!</h1>
       <p>
-        <a href="mailto:ugudeit@gmail.com">ugudeit@gmail.com</a>
+        <a href="mailto:ugudeit@gmail.com"><h2>ugudeit@gmail.com </h2> </a>
       </p>
+      <p><a href="https://www.linkedin.com/in/ugudeit/"><h2> LinkedIn </h2></a> </p>
+      <p><a href="https://www.github.com/ugudei/"><h2> Git Hub </h2></a> </p>
+
+  
     </Layout>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,24 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
 import gif from "../styles/echoSales.gif"
+import { Link } from "gatsby"
+
 export default function Projects() {
   return (
     <Layout>
-      <h1> Echo Sales </h1>
-      <p>
-      + Dynamic components, Node Server, SQL database, Jest testing
-      + Worked on micro services of item detail page, and built the dynamic components 
-      + Agile Development
-      + Deployed each of the micro services to AWS EC2 instance and built the combined proxy server
-      
-      </p>
+      <h1> <a href="https://github.com/FEC-ghrden02/landing-page">Echo Sales</a> </h1>
       <img src={gif}></img>
-      <h1> Car Carousel </h1>
       <p>
-      + Inherited legacy code with Redux, modified API calls
-      + Tested non-relational and relational database for efficiency using Artillery and New Relic, changed the existing database to MariaDB
-      + Generated 10 million records
-      + Using Docker, pushed containerized image to ECR, deployed to AWS and RWS
+      <dd> Dynamic components, Node Server, SQL database, Jest testing </dd>
+      <dd> Worked on micro services of item detail page, and built the dynamic components </dd>
+      <dd> Agile Development </dd>
+      <dd> Deployed each of the micro services to AWS EC2 instance and built the combined proxy server </dd>
+    
+      </p>
+      
+      <h1> <a href="https://github.com/SDC-den02/car-carousel"> Car Carousel </a> </h1>
+      <p>
+      <dd> Tested non-relational and relational database for efficiency using Artillery and New Relic, changed the existing database to MariaDB </dd>
+      <dd> Generated 10 million records </dd>
+      <dd> Inherited legacy code with Redux, modified API calls </dd>
+      <dd> Using Docker, pushed containerized image to ECR, deployed to AWS and RWS </dd>
       </p>
     </Layout>)
 }
